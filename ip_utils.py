959e3b3f445af5f21dbf99dddac6fcce9b4df4dc
liown <donghrel@163.com>
2022-07-07 14:21:48 +0800

    add human size

--- ip_utils.py
+++ ip_utils.py
@@ -24,3 +24,16 @@ def ip2int(ip):
 
 def int2ip(int_ip):
     return socket.inet_ntoa(struct.pack("!I", int_ip))
+
+
+
+import math
+
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
